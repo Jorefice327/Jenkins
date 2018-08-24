8bc9a9e1f0b538b24b6746c76838acc7462abee1
JOrefice <John.Orefice@emerson.com>
2018-08-24 14:08:12 -0600

    Hello git

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -1,7 +1,6 @@
 public class JenkinsTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("this worked?");
+        System.out.println("Hello git");
     }
 }
